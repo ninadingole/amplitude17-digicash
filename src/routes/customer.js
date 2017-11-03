b874665ac30a9083dca586e11924f5e66b2d287e
Ninad Ingole <ninad.ingole@gmail.com>
2017-11-03 20:21:44 +0530

    Enabled sns

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -9,58 +9,55 @@ var sns = new AWS.SNS();
 
 
 var router = function(nav, Customer) {
-    var customerController = require('../controller/CustomerController')(Customer);
-    customerRoute.route('/').get(function(req, res) {
-        res.render('generateQr', {
-            title: 'Add New QR code',
-            nav: nav
-        });
-    }).post(function(req, res) {
-        var qrCode = new qrModel(req.body);
-        console.log(qrCode.phone);
-        qrCode.qrId = uuidv4();
-        qrCode.pin = gpc(6);
-        var params = {
-            Message: 'Your Pin for Qr Cash is ' + qrCode.pin + ' form Amount: ' + qrCode.amount + 'INR.',
-            MessageStructure: 'string',
-            PhoneNumber: qrCode.phone
-        };
-
-        qrCode.save(function(err) {
-            if (err) {
-                res.status(500).send(err);
-            }
-            res.json({
-                'transaction_id': qrCode.qrId,
+        var customerController = require('../controller/CustomerController')(Customer);
+        customerRoute.route('/').get(function(req, res) {
+            res.render('generateQr', {
+                title: 'Add New QR code',
+                nav: nav
             });
-        });
+        }).post(function(req, res) {
+                var qrCode = new qrModel(req.body);
+                console.log(qrCode.phone);
+                qrCode.qrId = uuidv4();
+                qrCode.pin = gpc(6);
+                var params = {
+                    Message: 'Your Pin for Qr Cash is ' + qrCode.pin + ' form Amount: ' + qrCode.amount + 'INR.',
+                    MessageStructure: 'string',
+                    PhoneNumber: qrCode.phone
+                };
 
-        // sns.publish(params, function(err, data) {
-        //     if (err) {
-        //         res.status(500).send(err);
-        //     } else {
-        //         res.json({
-        //             'transaction_id': qrCode.qrId,
-        //         });
-        //     }
-        // });
+                qrCode.save(function(err) {
+                        if (err) {
+                            res.status(500).send(err);
+                        }
+                        sns.publish(params, function(err, data) {
+                                if (err) {
+                                    res.status(500).send(err);
+                                });
 
-    });
+                            res.json({
+                                'transaction_id': qrCode.qrId,
+                            });
+                        });
 
-    customerRoute.route('/:id').get(function(req, res) {
-        customerController.getById(req.params.id, function(err, customer) {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.render('generateQr', {
-                    customer: customer,
-                    nav: nav,
-                    title: 'Genrate Qr'
+
+
                 });
-            }
-        });
-    });
 
-    return customerRoute;
-};
-module.exports = router;
+            customerRoute.route('/:id').get(function(req, res) {
+                customerController.getById(req.params.id, function(err, customer) {
+                    if (err) {
+                        res.status(500).send(err);
+                    } else {
+                        res.render('generateQr', {
+                            customer: customer,
+                            nav: nav,
+                            title: 'Genrate Qr'
+                        });
+                    }
+                });
+            });
+
+            return customerRoute;
+        };
+        module.exports = router;
